3bb927a1ec77989fe0b02ad5ee1767f317162a62
WarWolf89 <almasi.csaba89@gmail.com>
2017-01-12 15:20:47 +0100

    a2

--- JasperReload/JasperPractice/src/playerA.java
+++ JasperReload/JasperPractice/src/playerA.java
@@ -2,7 +2,7 @@
  * Created by almasics on 2017.01.12..
  */
 public class playerA {
-    public void greet() {
-        System.out.println("greetings");
+    public void greet(String param) {
+        System.out.println(param);
     }
 }
