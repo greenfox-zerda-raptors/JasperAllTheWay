0b3131ea76c5eddab148756a957fd7065488a9bc
Adam <veroci@gmail.com>
2017-01-12 16:51:09 +0100

    c5

--- JasperReload/JasperPractice/src/PlayerC.java
+++ JasperReload/JasperPractice/src/PlayerC.java
@@ -2,8 +2,8 @@
  * Created by Verőci Ádám on 2017.01.12..
  */
 public class PlayerC {
-    public void cheer(String c) {
+    public void cheer(String c, String cc) {
         System.out.println(c);
-        System.out.println("szevasz tibi");
+        System.out.println(cc);
     }
 }
