c0ce50552674ee9aadd0a0f2394daa93470c71b0
WarWolf89 <almasi.csaba89@gmail.com>
2017-01-12 15:54:31 +0100

    bbb

--- JasperReload/JasperPractice/src/playerD.java
+++ JasperReload/JasperPractice/src/playerD.java
@@ -5,6 +5,7 @@ public class playerD {
 
     public void papgajfuture(long horoscope){
         System.out.println(horoscope);
+        System.out.println("kjashdkja");
     }
 
     public void orbitálisfaszság(){
