849011b2a795f5df2bd5e05860bd48e34b40a405
Adam <veroci@gmail.com>
2017-01-12 16:01:46 +0100

    conflict

--- JasperReload/JasperPractice/src/playerA.java
+++ JasperReload/JasperPractice/src/playerA.java
@@ -2,7 +2,7 @@
  * Created by almasics on 2017.01.12..
  */
 public class playerA {
-    public void greet() {
-        System.out.println("Greetings");
+    public void greet(String a) {
+        System.out.println(a);
     }
 }
