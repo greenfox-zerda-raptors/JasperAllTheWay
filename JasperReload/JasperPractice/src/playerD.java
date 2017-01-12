d4b90dc3c364914e05536af8c740b031ef039502
Z-Hamza <hamzazoltan@citromail.hu>
2017-01-12 17:08:16 +0100

    test

--- JasperReload/JasperPractice/src/playerD.java
+++ JasperReload/JasperPractice/src/playerD.java
@@ -3,7 +3,8 @@
  */
 public class playerD {
     
-    public void papgajfuture(int horoscope) {
+    public void papgajfuture(int horoscope, int alma) {
         System.out.println(horoscope);
+        System.out.println(alma);
     }
 }
